be30ddc4087b055004846789d2e636980eb1d468
Eoin Campbell <campbeeo@tcd.ie>
2015-10-29 12:54:43 +0000

    Initial version with ssmtp instead of postfix

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-docker-moodle
-=============
+docker-moodle-ssmtp
+===================
 
-A Dockerfile that installs the latest Moodle, Apache, PHP, MySQL and SSH
+Moodle on an Amazon AMI with Apache, PHP, MySQL and ssmtp
 
 ## Installation
 
 ```
-git clone https://github.com/sergiogomez/docker-moodle.git
+git clone https://github.com/ecampbell/docker-moodle.git
 cd docker-moodle
 docker build -t moodle .
 ```
@@ -16,13 +16,34 @@ docker build -t moodle .
 To spawn a new instance of Moodle:
 
 ```
-docker run --name moodle1 -e VIRTUAL_HOST=moodle.domain.com -d -t -p 80 -p 22 moodle
+docker run --name moodle1 -e VIRTUAL_HOST=moodle.domain.com -e MAIL_HOST=email-smtp.us-east-1.amazonaws.com:465 -e APACHE_PORT=81 -e AUTH_USER=MAILUSER -e AUTH_PASS=MAILPASSWORD --expose=81 -d -t -p 81:81 moodle3b_ssmtp
 ```
 
 You can visit the following URL in a browser to get started:
 
 ```
-http://moodle.domain.com/moodle
+http://moodle.domain.com/
 ```
 
-Thanks to [eugeneware](https://github.com/eugeneware) and [ricardoamaro](https://github.com/ricardoamaro) for their Dockerfiles.
+## Implementation details
+
+This configuration is tuned for use on an Amazon AMI, using the Amazon SES (Simple Email Service)
+to send out-bound emails from the server for user registration, news, etc.
+
+The SSH service is disabled on the instance, as this seems to be good practice, but it can be
+easily re-instated by uncommenting it.
+
+## Pre-requisites
+
+You must have an Amazon account, and also register for the Amazon SES service in order to
+get authentication details to be able to send emails.
+
+You can choose not to use the SES service, but another email provider instead. 
+However, you'll have to figure out the exact email configuration yourself, and it's
+not trivial.
+
+
+
+## Acknowledgements
+
+Thanks to [sergiogomez](https://github.com/sergiogomez) for his Dockerfile.
--- start.sh
+++ start.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 if [ ! -f /var/www/html/moodle/config.php ]; then
   #mysql has to be started this way as it doesn't work to call from /etc/init.d
-  /usr/bin/mysqld_safe & 
+  /usr/bin/mysqld_safe &
   sleep 10s
   # Here we generate random passwords (thank you pwgen!). The first two are for mysql users, the last batch for random keys in wp-config.php
   MOODLE_DB="moodle"
   MYSQL_PASSWORD=`pwgen -c -n -1 12`
   MOODLE_PASSWORD=`pwgen -c -n -1 12`
-  SSH_PASSWORD=`pwgen -c -n -1 12`
-  #This is so the passwords show up in logs. 
+  # SSH_PASSWORD=`pwgen -c -n -1 12`
+  #This is so the passwords show up in logs.
   echo mysql root password: $MYSQL_PASSWORD
   echo moodle password: $MOODLE_PASSWORD
-  echo ssh root password: $SSH_PASSWORD
-  echo root:$SSH_PASSWORD | chpasswd
+  # echo ssh root password: $SSH_PASSWORD
+  # echo root:$SSH_PASSWORD | chpasswd
   echo $MYSQL_PASSWORD > /mysql-root-pw.txt
   echo $MOODLE_PASSWORD > /moodle-db-pw.txt
-  echo $SSH_PASSWORD > /ssh-pw.txt
+  # echo $SSH_PASSWORD > /ssh-pw.txt
 
   sed -e "s/pgsql/mysqli/
   s/username/moodle/
@@ -23,7 +23,7 @@ if [ ! -f /var/www/html/moodle/config.php ]; then
   s/example.com/$VIRTUAL_HOST/
   s/\/home\/example\/moodledata/\/var\/moodledata/" /var/www/html/moodle/config-dist.php > /var/www/html/moodle/config.php
 
-  sed -i 's/PermitRootLogin without-password/PermitRootLogin Yes/' /etc/ssh/sshd_config
+  # sed -i 's/PermitRootLogin without-password/PermitRootLogin Yes/' /etc/ssh/sshd_config
 
   chown www-data:www-data /var/www/html/moodle/config.php
 
@@ -31,6 +31,24 @@ if [ ! -f /var/www/html/moodle/config.php ]; then
   mysql -uroot -p$MYSQL_PASSWORD -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION; FLUSH PRIVILEGES;"
   mysql -uroot -p$MYSQL_PASSWORD -e "CREATE DATABASE moodle; GRANT ALL PRIVILEGES ON moodle.* TO 'moodle'@'localhost' IDENTIFIED BY '$MOODLE_PASSWORD'; FLUSH PRIVILEGES;"
   killall mysqld
+
+  # Configure ssmtp to forward mail to Amazon SES SMTP server
+  # cf. http://edoceo.com/howto/ssmtp#ses
+  if [ ! -z "$MAIL_HOST" -a ! -z "$VIRTUAL_HOST" -a ! -z "$AUTH_USER" -a ! -z "$AUTH_PASS" ]
+  then
+    sed -ri -e "s/^(mailhub=).*/\1$MAIL_HOST/" \
+      -e "s/^#(rewriteDomain=)/\1$VIRTUAL_HOST/" \
+      -e "s/^(hostname=).*/\1$VIRTUAL_HOST/" \
+      -e "/^(hostname=)/ s/$/\nAuthUser=$AUTH_USER\nAuthPass=$AUTH_PASS\nUseTLS=YES/" \
+      -e "s/^#(FromLineOverride)/\1/" /tmp/ssmtp.conf
+  fi
+
+
+  if [ ! -z "$APACHE_PORT" ]
+  then
+    sed -ri -e "s/^(Listen).*/\1 $APACHE_PORT/" /tmp/ports.conf
+  fi
+
 fi
 # start all the services
 /usr/local/bin/supervisord -n
